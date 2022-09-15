83dfeeac1a0ff95ac8604896cf80316effedee86
Dileep Revanasiddappa <revan004@umn.edu>
2022-09-14 18:33:47 -0700

    Solve question 03
    Signed-off-by: Dileep Revanasiddappa <revan004@umn.edu>

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  if (string.length <= 1) {
+    return string;
+  }
+  let start = 0;
+  let end = string.length - 1;
+  let chars = string.split("");
+  while (start < end) {
+    let temp = chars[start];
+    chars[start] = chars[end];
+    chars[end] = temp;
+    start += 1;
+    end -= 1;
+  }
+  return chars.join("");
 };
 
 // Do not edit below this line
