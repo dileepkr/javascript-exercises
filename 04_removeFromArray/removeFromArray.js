3aeb3ae18119536b3d308f11946dd1764ae97bcb
Dileep Revanasiddappa <revan004@umn.edu>
2022-09-15 17:29:57 -0700

    Solve quesiton 04
    Signed-off-by: Dileep Revanasiddappa <revan004@umn.edu>

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  // remove 1 element from array
+  //   function removeElement(array, element) {
+  //     const index = array.indexOf(element);
+  //     if (index > -1) {
+  //       array.splice(index, 1);
+  //     }
+  //     return array;
+  //   }
+  //   for (const element of args) {
+  //     array = removeElement(array, element);
+  //   }
+  //   return array;
+  return array.filter((x) => !args.includes(x));
 };
 
 // Do not edit below this line
