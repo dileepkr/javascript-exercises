a0852f5f11ceeade320e4de094c2699e4098b8a5
Dileep Revanasiddappa <revan004@umn.edu>
2022-09-24 21:23:44 -0700

    solve question 07
    Signed-off-by: Dileep Revanasiddappa <revan004@umn.edu>

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (temperature) {
+  return +((temperature - 32) * (5 / 9)).toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function (temperature) {
+  return +(temperature * (9 / 5) + 32).toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
